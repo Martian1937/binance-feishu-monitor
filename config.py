27382b334a8af2d47e14730cee5152ce8d1b5b2e
Brave <claypoet1@gmail.com>
2026-05-25 16:23:17 +0800

    chore: remove LARK_WEBHOOK URL for security reasons

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ INTERVAL = "15m"
 THRESHOLD = 0.03
 CHECK_INTERVAL = 300
 ALERT_COOLDOWN = 1800
-LARK_WEBHOOK = "https://open.feishu.cn/open-apis/bot/v2/hook/bfe1c448-6800-41ec-8d82-c96d22fa7005"
+LARK_WEBHOOK = ""
 REST_URL = "https://fapi.binance.com/fapi/v1/klines"
